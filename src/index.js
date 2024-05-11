17b63c4f7086653ab9a711393e5e03354a1d0374
roee <roeeavir@gmail.com>
2024-05-11 20:09:14 +0300

    Configure React app for deployment to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <SurveyRoute />
+    {/* <RouterProvider router={router} /> */}
   </React.StrictMode>
 );
 
