07a48dd797c3e90ab3a8705d7c13daf2e932de67
roee <roeeavir@gmail.com>
2024-06-21 18:15:03 +0300

    Added mock login. Change login in routing and added sub routes for surveys and results

--- src/modules/notification/NotLoggedNotification.jsx
+++ src/modules/notification/NotLoggedNotification.jsx
@@ -3,7 +3,9 @@ import Notification from './Notification';
 
 const NotLoggedNotification = () => {
     return (
-        <Notification text={"Please log in to continue"}/>
+        <Notification text={"Please log in to continue"}
+            style={{color: "red", fontWeight: "600", fontSize: "2rem", textAlign: "center"}}
+        />
     );
 };
 
--- src/modules/notification/Notification.jsx
+++ src/modules/notification/Notification.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Notification.css';
 
-const Notification = ({text}) => {
+const Notification = ({text, style}) => {
     return (
-        <div className='notification'>
+        <div className='notification' style={style}>
             {text}
         </div>
     );
